109aab8df1957df27fc9bda23a91abc9851d4132
Kleber Augusto de Barros Silva <kleber.barros@einstein.br>
2022-12-29 01:54:44 -0300

    main

---
+++ script.js
@@ -0,0 +1,8 @@
+function soma() {
+    var nomerua=document.getElementById("endereco").value
+    var numerorua=document.getElementById("numero").value
+    console.log(nomerua)
+    console.log(numerorua)
+
+    document.getElementById("text_area").innerText=nomerua +','+ numerorua 
+}
